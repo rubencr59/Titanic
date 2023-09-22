8413da5c1f008ffe29702efaaf01c33b6be350ef
rubencr59 <123578705+rubencr59@users.noreply.github.com>
2023-09-22 10:02:36 +0200

    Delete src/main/kotlin/Tarea.kt

--- src/main/kotlin/Tarea.kt
+++
@@ -1,7 +0,0 @@
-data class Tarea(val Titulo:String, val Descripcion:String, val Dificultad:String){
-    var Completada:Boolean = false
-
-    override fun toString(): String {
-        return "Titulo: $Titulo \n Descripcion: $Descripcion \n Dificultad: $Dificultad \n Estado: $Completada \n"
-    }
-}
